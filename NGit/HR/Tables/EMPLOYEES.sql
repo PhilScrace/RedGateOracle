9c04394b5c5a041ea4bcb1b3c5944cf1e1302a1e
PhilScrace <phil.scrace@red-gate.com>
2015-10-16 13:11:15 +0100

    Update EMPLOYEES.sql

--- NGit/HR/Tables/EMPLOYEES.sql
+++ NGit/HR/Tables/EMPLOYEES.sql
@@ -10,6 +10,7 @@ CREATE TABLE hr.employees (
   commission_pct NUMBER(2,2),
   manager_id NUMBER(6),
   department_id NUMBER(4),
+  manager_od NUMBER(6),
   CONSTRAINT emp_email_uk UNIQUE (email),
   CONSTRAINT emp_emp_id_pk PRIMARY KEY (employee_id),
   CONSTRAINT emp_dept_fk FOREIGN KEY (department_id) REFERENCES hr.departments (department_id),
